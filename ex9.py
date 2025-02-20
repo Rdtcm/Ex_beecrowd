52c365f633237bb443ae10b6e2f30db79c914eac
Ryan <164410629+Rdtcm@users.noreply.github.com>
2025-02-20 11:56:28 -0300

    Update ex9.py

--- ex9.py
+++ ex9.py
@@ -1,12 +1,7 @@
 
 
-def main():
-    casos_teste = int(input().strip())
+casos_teste = int(input().strip())
     for _ in range(casos_teste):
         msg = input().lower().strip()
         msg_tratada = ''.join([palavra[0] for palavra in msg.split()])
         print(msg_tratada)
-
-
-if __name__ == '__main__':
-    main()
