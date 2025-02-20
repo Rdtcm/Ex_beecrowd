86056b84bb69564d52680b451ffa8f2309693b08
Ryan <164410629+Rdtcm@users.noreply.github.com>
2025-02-20 12:05:15 -0300

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,10 +1,7 @@
 #BEECROWD 1984
 
-def main():
-    num = input().strip() #considerando como string
-    num_invertido = num[::-1]
-    print(f"{num_invertido}")
 
+num = input().strip() #considerando como string
+num_invertido = num[::-1]
+print(f"{num_invertido}")
 
-if __name__ == '__main__':
-    main()
